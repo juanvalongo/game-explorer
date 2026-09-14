57f9ff7c716a0350b0db8ab6678b320fb381fed8
Juan Valongo <juan.25.silva@gmail.com>
2026-09-13 22:30:58 -0300

    corrige fallback das rotas no GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { copyFileSync } from 'node:fs'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   base: '/game-explorer/',
+
+  build: {
+    rollupOptions: {
+      plugins: [
+        {
+          name: 'gerar-404',
+
+          closeBundle() {
+            copyFileSync('dist/index.html', 'dist/404.html')
+          },
+        },
+      ],
+    },
+  },
 })
